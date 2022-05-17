94ec789d1ecb80e8a715bdd06568ae60a3fd8a09
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-17 15:35:57 -0300

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 
 
-function App() {
+const  App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Hello World com React com Material UI</h1>
     </div>
   );
 }
