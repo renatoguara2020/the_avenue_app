c794b0ec3a6369384924589d8747a0d398f5f094
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-18 10:45:13 -0300

    commit changes

--- src/components/featured/timeUntil.js
+++ src/components/featured/timeUntil.js
@@ -2,7 +2,12 @@ import React,{ useState,useEffect,useCallback } from 'react';
 import { Slide } from 'react-awesome-reveal';
 
 const TimeUntil = () => {
-     
+    const [time,setTime] = useState({
+        days:'0',
+        hours:'0',
+        minutes:'0',
+        second:'0'
+    })
 
     const renderItem = (time,value) => (
         <div className="countdown_item">
@@ -15,25 +20,48 @@ const TimeUntil = () => {
         </div>
     )
 
+    const getTimeUntil = useCallback( (deadline) => {
+        const time = Date.parse(deadline) - Date.parse(new Date());
+        if(time < 0){
+            console.log('Date passed')
+        } else{
+            const seconds = Math.floor((time/1000)%60);
+            const minutes = Math.floor((time/1000/60)%60);
+            const hours = Math.floor((time/(1000*60*60))%24);
+            const days = Math.floor((time/(1000*60*60*24)));
+
+            setTime({
+                days,
+                hours,
+                minutes,
+                seconds
+            })
+        }  
+    },[])
     
+    
+   
+
+
+    useEffect(()=>{
+        setInterval(()=> getTimeUntil('Jun, 10, 2022, 01:20:00'),1000)
+    },[getTimeUntil])
+
 
     return(
         <Slide left delay={1000}>
-        <div className="countdown_wrapper">
-            <div className="countdown_top">
-                Event starts in
+            <div className="countdown_wrapper">
+                <div className="countdown_top">
+                    Event starts in
+                </div>
+                <div className="countdown_bottom">
+                    {renderItem(time.days,'Days')}
+                    {renderItem(time.hours,'HS')}
+                    {renderItem(time.minutes,'Min')}
+                    {renderItem(time.seconds,'Sec')}
+                </div>
             </div>
-            <div className="countdown_bottom">
-                {renderItem(27,'Days')}
-                {renderItem(4,'HS')}
-                {renderItem(10,'Min')}
-                {renderItem(50,'Sec')}
-
-
-
-            </div>
-        </div>
-    </Slide>
+        </Slide>
     )
 
 }
