8cb6c10e4982e310f45a5c0b94e25d6aa90320e9
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-18 08:22:42 -0300

    commit changes

---
+++ src/components/featured/Carrousel.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Slider from 'react-slick';
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import slide_one from '../../resources/images/slide_one.jpg';
+import slide_two from '../../resources/images/slide_two.jpg';
+import slide_three from '../../resources/images/slide_three.jpg';
+
+
+const Carrousel = () =>{
+
+    return(
+
+    <div className="carrousel_wrapper" 
+    style= {{height: `${window.innerHeight}px`}}>
+
+    </div>
+
+    )
+
+}
+
+export default Carrousel;
---
+++ src/components/featured/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Featured = () =>{
+
+    return(
+
+        <div className="featured_container">
+        
+        </div>
+    )
+}
+
+export default Featured;
