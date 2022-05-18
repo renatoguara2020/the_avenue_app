0a92396f461ee69bbf9db849dea6e889991bd5d9
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-18 13:50:45 -0300

    commit changes

--- src/components/highlights/Discount.js
+++ src/components/highlights/Discount.js
@@ -4,14 +4,45 @@ import {Fade, Slide} from 'react-awesome-reveal';
 
 const Discount = () =>{
 
+    const end = 30;
+    const [start, setStart] = useState(0)
+    const porcentage = () =>{
 
+        if(start < end){
+
+            setStart(prevCount => prevCount + 1)
+        }
+
+
+    }
+
+
+
+    useEffect(()=>{
+
+    if(start > 0 && start < 30){
+     setTimeout(()=>{
+
+   setStart(prevCount => prevCount + 1)
+     },30)
+
+    }
+
+    },[start])
     return(
 
     <div className="center_wrapper">
       <div className="discount_wrapper">
-       <Fade>
+       <Fade onVisibilityChange={(inView)=>{
+
+           if(inView){
+
+            porcentage()
+           }
+
+       }}>
         <div className="discount_porcentage">
-        <span>30%</span>
+        <span>{start}</span>
         <span>OFF</span>
         </div>
        </Fade>
