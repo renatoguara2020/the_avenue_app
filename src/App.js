789e5feb4944aafec813a53a7e098a1b653d30b1
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-18 12:08:40 -0300

    commit changes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import './resources/styles.css';
 import Header from './components/header_footer/Header';
 import Footer from './components/header_footer/Footer';
 import Featured from './components/featured';
-
+import VenueNfo from './components/venueNfo';
+import {Fragment}  from 'react';
 const  App = () => {
   return (
-    
+
     <div className="App">
        
        <Header />
-       <>
+       <Fragment>
        <Featured/>
-       </>
+       </Fragment>
+
+       <Fragment>
+        <VenueNfo />
+       </Fragment>
         <div style={{backgroundColor:'red', height:'800px'}}></div>
         <div style={{backgroundColor:'blue', height:'800px'}}></div>
         <div style={{backgroundColor:'green', height:'800px'}}></div>
