2ffe3c9bea35c2eb5a901be3029749931f99d4f7
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-18 06:42:20 -0300

    commit changes

--- src/components/header_footer/Footer.js
+++ src/components/header_footer/Footer.js
@@ -1,11 +1,14 @@
 import React from "react";
+import {Fade}  from 'react-awesome-reveal';
 
 const Footer = () => {
   return (
-    <footer className="bck_red">
+  <footer className="bck_red">
+    <Fade>
       <div className="font_heighteous footer_logo_venue">The Venue</div>
       <div className="footer_copyright">The Venue 2022 all rights reserved</div>
-    </footer>
+    </Fade>
+  </footer>
   );
 };
 
